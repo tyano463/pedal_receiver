12a59d1c58aaac4e19d647dc316cdb7ee389acc3
Yano, Takayuki <ta_yano@aol.com>
2025-06-04 16:19:11 +0900

    g bluez

--- src/main.c
+++ src/main.c
@@ -1,362 +1,144 @@
 #include <gio/gio.h>
-#include <glib.h>
+#include <stdlib.h>
 #include <stdio.h>
-#include "v_common.h"
+#include <string.h>
 #include "v_log.h"
 
-#define TARGET_UUID "0000xxxx-0000-1000-8000-00805f9b34fb"      // 目的のサービスUUID
-#define TARGET_CHAR_UUID "0000yyyy-0000-1000-8000-00805f9b34fb" // 目的のキャラクタリスティックUUID
+#define TARGET_UUID "2ae0ffc2-dd80-4bab-a3a2-01c6f6193761"
 
-static void on_characteristic_changed(GDBusConnection *connection,
-                                      const gchar *sender_name,
-                                      const gchar *object_path,
-                                      const gchar *interface_name,
-                                      const gchar *signal_name,
-                                      GVariant *parameters,
-                                      gpointer user_data);
+static GMainLoop *main_loop;
 
-static GDBusConnection *conn = NULL;
-static gchar *target_device_path = NULL;
-
-// スキャンの開始
-void start_discovery()
-{
-    g_dbus_connection_call(conn,
-                           "org.bluez",
-                           "/org/bluez/hci0",
-                           "org.bluez.Adapter1",
-                           "StartDiscovery",
-                           NULL,
-                           NULL,
-                           G_DBUS_CALL_FLAGS_NONE,
-                           -1,
-                           NULL,
-                           NULL, NULL);
-}
-
-// スキャンの停止
-void stop_discovery()
-{
-    g_dbus_connection_call(conn,
-                           "org.bluez",
-                           "/org/bluez/hci0",
-                           "org.bluez.Adapter1",
-                           "StopDiscovery",
-                           NULL,
-                           NULL,
-                           G_DBUS_CALL_FLAGS_NONE,
-                           -1,
-                           NULL,
-                           NULL, NULL);
-}
-
-// デバイスの接続
-void connect_device(const gchar *device_path)
-{
-    g_dbus_connection_call(conn,
-                           "org.bluez",
-                           device_path,
-                           "org.bluez.Device1",
-                           "Connect",
-                           NULL,
-                           NULL,
-                           G_DBUS_CALL_FLAGS_NONE,
-                           -1,
-                           NULL,
-                           NULL, NULL);
-}
-
-// ServicesResolved プロパティの変更を監視
-void on_properties_changed(GDBusConnection *connection,
-                           const gchar *sender_name,
-                           const gchar *object_path,
-                           const gchar *interface_name,
-                           const gchar *signal_name,
-                           GVariant *parameters,
-                           gpointer user_data)
-{
-    GVariantIter *props;
-    const gchar *iface;
-    GVariant *value;
-    g_variant_get(parameters, "(&sa{sv}as)", &iface, &props, NULL);
-    while (g_variant_iter_next(props, "{&sv}", &iface, &value))
-    {
-        if (g_strcmp0(iface, "ServicesResolved") == 0 && g_variant_get_boolean(value))
-        {
-            // サービスが解決されたらキャラクタリスティックを探索
-            // ここでキャラクタリスティックの探索と通知の有効化を行う
-        }
-        g_variant_unref(value);
-    }
-    g_variant_iter_free(props);
-}
-
-void enable_notify(GDBusConnection *conn, const char *char_path)
-{
-    // Notify有効化
-    g_dbus_connection_call(
-        conn,
-        "org.bluez",
-        char_path,
-        "org.bluez.GattCharacteristic1",
-        "StartNotify",
-        NULL,
-        NULL,
-        G_DBUS_CALL_FLAGS_NONE,
-        -1,
-        NULL,
-        NULL,
-        NULL);
-
-    // PropertiesChangedシグナルにコールバックを登録
-    g_dbus_connection_signal_subscribe(
-        conn,
-        "org.bluez",
-        "org.freedesktop.DBus.Properties",
-        "PropertiesChanged",
-        char_path,
-        NULL,
-        G_DBUS_SIGNAL_FLAGS_NONE,
-        on_characteristic_changed, // 別途実装済みのコールバック
-        NULL,
-        NULL);
-}
-char *find_gatt_service(GDBusConnection *conn, const char *adapter_path, const char *device_path, const char *target_uuid)
-{
-    GVariant *objects = g_dbus_connection_call_sync(
-        conn,
-        "org.bluez",
-        "/",
-        "org.freedesktop.DBus.ObjectManager",
-        "GetManagedObjects",
-        NULL,
-        G_VARIANT_TYPE("(a{oa{sa{sv}}})"),
-        G_DBUS_CALL_FLAGS_NONE,
-        -1,
-        NULL,
-        NULL);
-    if (!objects)
-        return NULL;
-
-    GVariantIter *iter;
-    g_variant_get(objects, "(a{oa{sa{sv}}})", &iter);
-
-    const char *object_path;
-    GVariant *interfaces;
-    while (g_variant_iter_next(iter, "{&oa{sa{sv}}}", &object_path, &interfaces))
-    {
-        if (!g_str_has_prefix(object_path, device_path))
-        {
-            g_variant_unref(interfaces);
-            continue;
-        }
-
-        GVariantIter iface_iter;
-        g_variant_iter_init(&iface_iter, interfaces);
-        const char *iface_name;
-        GVariant *props;
-        while (g_variant_iter_next(&iface_iter, "{&sa{sv}}", &iface_name, &props))
-        {
-            if (g_strcmp0(iface_name, "org.bluez.GattService1") == 0)
-            {
-                GVariant *uuid_var = g_variant_lookup_value(props, "UUID", NULL);
-                if (uuid_var && g_strcmp0(g_variant_get_string(uuid_var, NULL), target_uuid) == 0)
-                {
-                    g_variant_unref(uuid_var);
-                    g_variant_unref(props);
-                    g_variant_unref(interfaces);
-                    g_variant_iter_free(iter);
-                    g_variant_unref(objects);
-                    return g_strdup(object_path); // 呼び出し元で解放必要
-                }
-                g_variant_unref(uuid_var);
-            }
-            g_variant_unref(props);
-        }
-        g_variant_unref(interfaces);
-    }
-
-    g_variant_iter_free(iter);
-    g_variant_unref(objects);
-    return NULL;
-}
-
-char *find_gatt_characteristic(GDBusConnection *conn, const char *service_path, const char *target_char_uuid)
-{
-    GVariant *objects = g_dbus_connection_call_sync(
-        conn,
-        "org.bluez",
-        "/",
-        "org.freedesktop.DBus.ObjectManager",
-        "GetManagedObjects",
-        NULL,
-        G_VARIANT_TYPE("(a{oa{sa{sv}}})"),
-        G_DBUS_CALL_FLAGS_NONE,
-        -1,
-        NULL,
-        NULL);
-    if (!objects)
-        return NULL;
-
-    GVariantIter *iter;
-    g_variant_get(objects, "(a{oa{sa{sv}}})", &iter);
-
-    const char *object_path;
-    GVariant *interfaces;
-    while (g_variant_iter_next(iter, "{&oa{sa{sv}}}", &object_path, &interfaces))
-    {
-        if (!g_str_has_prefix(object_path, service_path))
-        {
-            g_variant_unref(interfaces);
-            continue;
-        }
-
-        GVariantIter iface_iter;
-        g_variant_iter_init(&iface_iter, interfaces);
-        const char *iface_name;
-        GVariant *props;
-        while (g_variant_iter_next(&iface_iter, "{&sa{sv}}", &iface_name, &props))
-        {
-            if (g_strcmp0(iface_name, "org.bluez.GattCharacteristic1") == 0)
-            {
-                GVariant *uuid_var = g_variant_lookup_value(props, "UUID", NULL);
-                if (uuid_var && g_strcmp0(g_variant_get_string(uuid_var, NULL), target_char_uuid) == 0)
-                {
-                    g_variant_unref(uuid_var);
-                    g_variant_unref(props);
-                    g_variant_unref(interfaces);
-                    g_variant_iter_free(iter);
-                    g_variant_unref(objects);
-                    return g_strdup(object_path); // 呼び出し元で解放必要
-                }
-                g_variant_unref(uuid_var);
-            }
-            g_variant_unref(props);
-        }
-        g_variant_unref(interfaces);
-    }
-
-    g_variant_iter_free(iter);
-    g_variant_unref(objects);
-    return NULL;
-}
-
-static void on_characteristic_changed(GDBusConnection *connection,
-                                      const gchar *sender_name,
-                                      const gchar *object_path,
-                                      const gchar *interface_name,
-                                      const gchar *signal_name,
-                                      GVariant *parameters,
-                                      gpointer user_data)
-{
-    GVariant *value;
-    const gchar *iface;
-    GVariantIter *props;
-    char buf[MAX_PATH], *p;
-    p = buf;
-
-    g_variant_get(parameters, "(&sa{sv}as)", &iface, &props, NULL);
-    while (g_variant_iter_next(props, "{&sv}", &iface, &value))
-    {
-        if (g_strcmp0(iface, "Value") == 0)
-        {
-            GVariantIter *array;
-            guchar byte;
-            g_variant_get(value, "ay", &array);
-            p = buf;
-            p += sprintf(p, "Notify data: ");
-            while (g_variant_iter_next(array, "y", &byte))
-            {
-                p += sprintf(p, "%02x ", byte);
-            }
-            d("%s", buf);
-            g_variant_iter_free(array);
-        }
-        g_variant_unref(value);
-    }
-    g_variant_iter_free(props);
-}
-// InterfacesAdded シグナルのハンドラ
-void on_interfaces_added(GDBusConnection *connection,
-                         const gchar *sender_name,
-                         const gchar *object_path,
-                         const gchar *interface_name,
-                         const gchar *signal_name,
-                         GVariant *parameters,
-                         gpointer user_data)
+static void on_interface_added(GDBusConnection *conn,
+                               const gchar *sender_name,
+                               const gchar *object_path,
+                               const gchar *interface_name,
+                               const gchar *signal_name,
+                               GVariant *parameters,
+                               gpointer user_data)
 {
     GVariantIter *interfaces;
     const gchar *object;
-    g_variant_get(parameters, "(&oa{sa{sv}})", &object, &interfaces);
-    d("%s", object);
-    if (g_str_has_prefix(object, "/org/bluez/hci0/dev_"))
+    const gchar *interface;
+    GVariantIter iter;
+    GVariant *properties;
+    GVariant *interfaces_variant;
+
+    g_variant_get(parameters, "(&o@a{sa{sv}})", &object, &interfaces_variant);
+    g_variant_iter_init(&iter, interfaces_variant);
+    while (g_variant_iter_loop(&iter, "{&s@a{sv}}", &interface_name, &properties))
     {
-        // Device1 インターフェースの UUIDs プロパティを取得
-        GVariant *result = g_dbus_connection_call_sync(conn,
-                                                       "org.bluez",
-                                                       object,
-                                                       "org.freedesktop.DBus.Properties",
-                                                       "Get",
-                                                       g_variant_new("(ss)", "org.bluez.Device1", "UUIDs"),
-                                                       NULL,
-                                                       G_DBUS_CALL_FLAGS_NONE,
-                                                       -1,
-                                                       NULL,
-                                                       NULL);
+        if (g_strcmp0(interface_name, "org.bluez.Device1") != 0)
+            continue;
 
-        if (!result)
-            return;
-        GVariant *uuid_variant;
+        d("name: %s", interface_name);
+        gchar *uuid;
+        GVariant *uuids = g_variant_lookup_value(properties, "UUIDs", G_VARIANT_TYPE("as"));
+        if (!uuids)
+            continue;
+
         GVariantIter iter;
-        const gchar *uuid;
-        g_variant_get(result, "(v)", &uuid_variant);
-        g_variant_iter_init(&iter, uuid_variant);
-
+        g_variant_iter_init(&iter, uuids);
         while (g_variant_iter_loop(&iter, "s", &uuid))
         {
-            d("  UUID: %s", uuid);
-            if (g_strcmp0(uuid, TARGET_UUID) == 0)
+            if (g_ascii_strcasecmp(uuid, TARGET_UUID) == 0)
             {
-                stop_discovery();
-                connect_device(object);
-                break;
+                g_print("Found device %s with target UUID!\n", object);
+                GDBusProxy *device_proxy = g_dbus_proxy_new_sync(
+                    conn,
+                    G_DBUS_PROXY_FLAGS_NONE,
+                    NULL,
+                    "org.bluez",
+                    object,
+                    "org.bluez.Device1",
+                    NULL,
+                    NULL);
+
+                GError *error = NULL;
+                g_dbus_proxy_call_sync(device_proxy, "Connect", NULL,
+                                       G_DBUS_CALL_FLAGS_NONE, -1, NULL, &error);
+                if (error)
+                {
+                    g_printerr("Failed to connect: %s\n", error->message);
+                    g_error_free(error);
+                }
+                else
+                {
+                    g_print("Connected to device.\n");
+                    g_main_loop_quit(main_loop); // 成功したら抜ける
+                }
+
+                g_object_unref(device_proxy);
             }
         }
+        g_variant_unref(uuids);
     }
-    g_variant_iter_free(interfaces);
+    g_variant_unref(interfaces_variant);
 }
 
-int main(int argc, char *argv[])
+int main()
 {
-    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
-    conn = g_bus_get_sync(G_BUS_TYPE_SYSTEM, NULL, NULL);
+    GDBusConnection *conn;
+    GError *error = NULL;
 
-    // InterfacesAdded シグナルの監視
-    g_dbus_connection_signal_subscribe(conn,
-                                       "org.bluez",
-                                       "org.freedesktop.DBus.ObjectManager",
-                                       "InterfacesAdded",
-                                       NULL,
-                                       NULL,
-                                       G_DBUS_SIGNAL_FLAGS_NONE,
-                                       on_interfaces_added,
-                                       NULL,
-                                       NULL);
+    v_log_init();
 
-    // PropertiesChanged シグナルの監視
-    g_dbus_connection_signal_subscribe(conn,
-                                       "org.bluez",
-                                       "org.freedesktop.DBus.Properties",
-                                       "PropertiesChanged",
-                                       NULL,
-                                       NULL,
-                                       G_DBUS_SIGNAL_FLAGS_NONE,
-                                       on_properties_changed,
-                                       NULL,
-                                       NULL);
+    conn = g_bus_get_sync(G_BUS_TYPE_SYSTEM, NULL, &error);
+    if (!conn)
+    {
+        g_printerr("Failed to connect to system bus: %s\n", error->message);
+        g_error_free(error);
+        return 1;
+    }
 
-    start_discovery();
-    g_main_loop_run(loop);
-    g_main_loop_unref(loop);
+    // スキャン開始
+    GDBusProxy *adapter_proxy = g_dbus_proxy_new_sync(
+        conn,
+        G_DBUS_PROXY_FLAGS_NONE,
+        NULL,
+        "org.bluez",
+        "/org/bluez/hci0",
+        "org.bluez.Adapter1",
+        NULL,
+        &error);
+
+    if (error)
+    {
+        g_printerr("Failed to create adapter proxy: %s\n", error->message);
+        g_error_free(error);
+        return 1;
+    }
+
+    g_dbus_proxy_call_sync(adapter_proxy, "StartDiscovery", NULL,
+                           G_DBUS_CALL_FLAGS_NONE, -1, NULL, &error);
+    d("");
+    if (error)
+    {
+        g_printerr("Failed to start discovery: %s\n", error->message);
+        g_error_free(error);
+        return 1;
+    }
+
+    g_print("Started BLE scan...\n");
+
+    // Signal subscription
+    guint signal_id = g_dbus_connection_signal_subscribe(
+        conn,
+        "org.bluez",
+        "org.freedesktop.DBus.ObjectManager",
+        "InterfacesAdded",
+        NULL,
+        NULL,
+        G_DBUS_SIGNAL_FLAGS_NONE,
+        on_interface_added,
+        NULL,
+        NULL);
+
+    main_loop = g_main_loop_new(NULL, FALSE);
+    g_main_loop_run(main_loop);
+
+    g_dbus_connection_signal_unsubscribe(conn, signal_id);
+    g_object_unref(adapter_proxy);
+    g_object_unref(conn);
+
     return 0;
 }
